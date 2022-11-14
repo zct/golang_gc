c2aab693ac56d93f32c8504abe4d19e2981bdd1d
tangzhongcheng <tangzhongcheng@kuaishou.com>
2022-11-14 16:36:59 +0800

    add

---
+++ sync_pool/benchmark_pool.go
@@ -0,0 +1,15 @@
+func BenchmarkPoolGC(b *testing.B) {
+	b.ReportAllocs()
+	var a, z [1000]*flate.Writer
+	p := sync.Pool{New: func() interface{} { return &flate.Writer{} }}
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < len(a); j++ {
+			a[j] = p.Get().(*flate.Writer)
+		}
+		for j := 0; j < len(a); j++ {
+			p.Put(a[j])
+		}
+		a = z
+		runtime.GC()
+	}
+}
